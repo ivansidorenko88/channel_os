615efb52b4f11e75851d5aafbe53429276b4cd74
Mausk <ivansidorenkox@gmail.com>
2026-07-08 06:54:15 +0300

    remove prisma for bothost

--- src/repositories/postRepository.js
+++
@@ -1,9 +0,0 @@
-const prisma = require("../config/prisma");
-async function createPublishedPost({ channelId, text, telegramMessageId }) {
-  return prisma.post.create({ data: { channelId, text, telegramMessageId, status: "published", publishedAt: new Date() } });
-}
-async function getPostStats(ownerId) {
-  const channels = await prisma.channel.findMany({ where: { ownerId }, include: { posts: true } });
-  return { channelCount: channels.length, totalPosts: channels.reduce((sum, ch) => sum + ch.posts.length, 0) };
-}
-module.exports = { createPublishedPost, getPostStats };
--- src/repositories/userRepository.js
+++
@@ -1,9 +0,0 @@
-const prisma = require("../config/prisma");
-async function upsertFromTelegramUser(from) {
-  return prisma.user.upsert({
-    where: { telegramId: String(from.id) },
-    update: { username: from.username || null, firstName: from.first_name || null, lastName: from.last_name || null },
-    create: { telegramId: String(from.id), username: from.username || null, firstName: from.first_name || null, lastName: from.last_name || null },
-  });
-}
-module.exports = { upsertFromTelegramUser };
